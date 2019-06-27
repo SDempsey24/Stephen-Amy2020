8cf7a01228f2f39ec54fd22249313547f5dcc795
Stephen <38697987+SDempsey24@users.noreply.github.com>
2019-06-27 09:38:30 +0100

    Update
    Updated entire look of the website. Still unfinished.

---
+++ newWedding.js
@@ -0,0 +1,10 @@
+<script type="text/javascript" defer src="//www.123formbuilder.com/embed/4789079.js" data-role="form" data-default-width="650px"></script>
+
+function toggleText() {
+    var text = document.getElementById("accom-btn");
+    if (text.style.display === "none") {
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+  }
