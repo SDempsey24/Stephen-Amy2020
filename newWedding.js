ba9dbdf39f561b8fa60c8f9125167139495fdd43
Stephen <38697987+SDempsey24@users.noreply.github.com>
2019-07-16 12:24:46 +0100

    Commented out function

--- newWedding.js
+++ newWedding.js
@@ -1,5 +1,5 @@
 <script type="text/javascript" defer src="//www.123formbuilder.com/embed/4789079.js" data-role="form" data-default-width="650px"></script>
-
+/*
 function toggleText() {
     var text = document.getElementById("accom-btn");
     if (text.style.display === "none") {
@@ -8,3 +8,4 @@ function toggleText() {
       text.style.display = "none";
     }
   }
+  */
